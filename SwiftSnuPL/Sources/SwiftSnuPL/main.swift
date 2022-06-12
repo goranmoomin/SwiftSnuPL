bda61d899283dd9a585b1d36aca979e8c34aec3e
Sungbin Jo <goranmoomin@daum.net>
2022-06-13 00:37:03 +0900

    Add compilation to main file

--- SwiftSnuPL/Sources/SwiftSnuPL/main.swift
+++ SwiftSnuPL/Sources/SwiftSnuPL/main.swift
@@ -1,35 +1,56 @@
 import Foundation
 
-let testsDirectoryURL = URL(fileURLWithPath: #file).deletingLastPathComponent()
-    .appendingPathComponent("../../../test/parser/").standardized
+let source = #"""
+    //
+    // test2
+    //
+    // IR generation
+    //
 
-for testSourceURL in try FileManager.default
-    .contentsOfDirectory(at: testsDirectoryURL, includingPropertiesForKeys: nil)
-    .filter({ $0.pathExtension == "mod" })
-{
-    print("parsing and resolving \(testSourceURL.lastPathComponent)...")
-    let sourceData = try Data(contentsOf: testSourceURL)
-    let scanner = Scanner(data: sourceData)
-    let parser = Parser(scanner: scanner)
-    guard let module = try? parser.parse() else {
-        for parseError in parser.errors {
-            if let token = parseError.token, let message = parseError.message {
-                print("error: \(token.lineNumber):\(token.charPosition) \(message)")
-            }
+    module test2;
+
+    var i: integer;
+
+    procedure foo(v: integer);
+    var i: integer;
+    begin
+      WriteInt(v)
+    end foo;
+
+    function bar(p1, p2, p3, p4: integer): integer;
+    begin
+      return p1 + p2 + p3 + p4
+    end bar;
+
+    begin
+      foo(bar(1, 2, 3, 4))
+    end test2.
+    """#
+
+
+let sourceData = source.data(using: .utf8)!
+let scanner = Scanner(data: sourceData)
+let parser = Parser(scanner: scanner)
+guard let module = try? parser.parse() else {
+    for parseError in parser.errors {
+        if let token = parseError.token, let message = parseError.message {
+            print("error: \(token.lineNumber):\(token.charPosition) \(message)")
         }
-        print("error: parsing failed.")
-        continue
     }
-    let resolver = Resolver(module: module)
-    guard (try? resolver.resolve()) != nil else {
-        print("error: resolving failed.")
-        continue
-    }
-
-    let generator = Generator(
-        module: module, resolvedSymbols: resolver.resolvedSymbols,
-        resolvedTypes: resolver.resolvedTypes)
-    let tac = generator.generate()
-    for (symbol, instructions) in tac { print(format(symbol: symbol, instructions: instructions)) }
-    print("succesfully resolved \(testSourceURL.lastPathComponent).")
+    print("error: parsing failed.")
+    exit(-1)
 }
+let resolver = Resolver(module: module)
+guard (try? resolver.resolve()) != nil else {
+    print("error: resolving failed.")
+    exit(-1)
+}
+
+let irGenerator = IRGenerator(
+    module: module, resolvedSymbols: resolver.resolvedSymbols, resolvedTypes: resolver.resolvedTypes
+)
+let tac = irGenerator.generate()
+let asmGenerator = AssemblyGenerator(
+    instructions: tac, allocations: irGenerator.allocations,
+    stringLiterals: irGenerator.stringLiterals)
+print(asmGenerator.generate())
