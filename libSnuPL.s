bfe3df138bf5847558b0a4d3daf6cc86afe2000b
Sungbin Jo <goranmoomin@daum.net>
2022-06-20 21:52:16 +0900

    Add ReadInt and ReadLong implementation

--- libSnuPL.s
+++ libSnuPL.s
@@ -1,10 +1,42 @@
 	.data
+ReadInt.str:
+	.asciz	"%d"
+ReadLong.str:
+	.asciz	"%lld"
 WriteLong.str:
-	.asciz	"%ld"
+	.asciz	"%lld"
 WriteStr.str:
 	.asciz	"%s"
 
 	.text
+	.globl ReadInt
+ReadInt:
+	sub	sp, sp, #16
+	adrp	x0, ReadLong.str
+	add	x0, x0, :lo12:ReadLong.str
+	add	x1, sp, #4
+	stp	x29, x30, [sp, #-16]!
+	mov	x29, sp
+	bl	scanf
+	ldp	x29, x30, [sp], #16
+	ldrsw	x0, [sp, #4]
+	add	sp, sp, #16
+	ret
+
+	.globl ReadLong
+ReadLong:
+	sub	sp, sp, #16
+	adrp	x0, ReadLong.str
+	add	x0, x0, :lo12:ReadLong.str
+	add	x1, sp, #8
+	stp	x29, x30, [sp, #-16]!
+	mov	x29, sp
+	bl	scanf
+	ldp	x29, x30, [sp], #16
+	ldr	x0, [sp, #8]
+	add	sp, sp, #16
+	ret
+
 	.globl WriteLong
 WriteLong:
 	mov	x1, x0
